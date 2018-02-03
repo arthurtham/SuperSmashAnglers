01567d4fdab356001d751f99ea84b3d437b3d590
arthurtham <19481191+arthurtham@users.noreply.github.com>
2018-02-03 02:26:39 -0800

    Added Restart Button and Player Prefab

---
+++ Assets/Scripts/ButtonRestart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonRestart : MonoBehaviour
+{
+	public Button m_Button;
+	//private GameObject Timer = GameObject.FindGameObjectWithTag("TimerTag");
+
+	void Start()
+	{
+		Button btn = m_Button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the restart button!");
+		SceneManager.LoadScene (this.gameObject.scene.name, LoadSceneMode.Single);
+	}
+}
