0f4fd23748815a8bc033a4f08150968faa0a3713
arthurtham <19481191+arthurtham@users.noreply.github.com>
2018-02-03 14:49:34 -0800

    Added Carl

---
+++ Assets/Scripts/ButtonPlayerSelect.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonPlayerSelect : MonoBehaviour {
+
+	//Reference itself
+	public static ButtonPlayerSelect instance;
+
+	private int playerId;
+	private static int playerIdMax = 2;
+	public int m_playerIdDefault;
+	public int player;
+	private static int[] playerIds = new int[playerIdMax+1];
+	public Button m_Button;
+
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		playerId =m_playerIdDefault;
+		updateSprite ();
+		Button btn = m_Button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		nextPlayer ();	
+		updateSprite ();
+	}
+
+	public int[] getPlayerIds() {
+		return playerIds;
+	}
+
+	void nextPlayer()
+	{
+		if (playerId >= playerIdMax) {
+			playerId = 0;
+		} else {
+			playerId++;
+		}
+		Debug.Log("playerId is now " + playerId.ToString() + "!");
+	}
+
+	void updateSprite() {
+		string playerName = idToString (playerId);
+		Debug.Log ("./Sprites/Players/" + playerName + ".png");
+		Sprite toLoad = Resources.Load<Sprite> ("Sprites/Players/"+playerName+"");
+		m_Button.GetComponent<Image> ().sprite = toLoad;
+		m_Button.GetComponentInChildren<Text> ().text = "Player "+(player+1).ToString()+": "+playerName;
+		playerIds [player] = playerId;
+
+	}
+
+	public string idToString(int id) {
+		switch (id) {
+		case 0:
+			return "Jerald";
+		case 1:
+			return "Jessica";
+		case 2:
+			return "Carl";
+		} 
+		return "null";
+	}
+}
---
+++ Assets/Scripts/PlayerGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGenerator : MonoBehaviour {
+
+	private static int[] playerIds;
+	private GameObject p1;
+	private GameObject p2;
+
+	// Use this for initialization
+	void Start () {
+		playerIds = ButtonPlayerSelect.instance.getPlayerIds();
+		Debug.Log (playerIds[0]);
+		p1 = GameObject.FindGameObjectWithTag ("Player1").gameObject;
+		p2 = GameObject.FindGameObjectWithTag ("Player2").gameObject;
+
+		StartCoroutine (LateStart ());
+
+	}
+
+	IEnumerator LateStart()
+	{
+		yield return new WaitForEndOfFrame ();
+		Generate ();
+	}
+
+	void Generate() {
+		Debug.Log (ButtonPlayerSelect.instance.idToString (playerIds [0]));
+		//Debug.Log (obj);
+
+		Sprite toLoad = Resources.Load<Sprite> ("Sprites/Players/"+ButtonPlayerSelect.instance.idToString (playerIds [0]));
+		p1.GetComponent<SpriteRenderer> ().sprite = toLoad;
+		p1.AddComponent<PolygonCollider2D> ();
+
+		toLoad = Resources.Load<Sprite> ("Sprites/Players/"+ButtonPlayerSelect.instance.idToString (playerIds [1]));
+		p2.GetComponent<SpriteRenderer>().sprite = toLoad;
+		p2.AddComponent<PolygonCollider2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
